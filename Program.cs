01635f10921261f4522939d7d15ca7690c74e990
IStillTry <bohdan.konstantynov@nure.ua>
2023-05-06 20:18:52 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,8 +13,6 @@ namespace Jewelry_shop
     {
         public static void Main(string[] args)
         {
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Console.InputEncoding = System.Text.Encoding.UTF8;
             CreateHostBuilder(args).Build().Run();
         }
 
